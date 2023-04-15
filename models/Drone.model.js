f52c4acefa1f8bba4f238b820a28be7932925339
Singabe Djonouma <singabe-xd@hotmail.fr>
2023-04-15 16:14:39 +0200

    iteration1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,12 @@
 // Iteration #1
+const mongoose = require("mongoose");
+
+const DroneSchema = new mongoose.Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+});
+
+const Drone = mongoose.model("Drone",DroneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,26 @@
+require('../db')
+
 // Iteration #1
+const mongoose = require("mongoose")
+const Drone = require("../models/Drone.model")
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+Drone.deleteMany()
+    .then(() => {
+        Drone.create(drones)
+            .then((createdDrones) => {
+                console.log(`${createdDrones.length} drones have been created`)
+
+                // fermer la connection avec la DB
+                mongoose.connection.close();
+            })
+    })
+    .catch(err => console.log(err))
+
+
+
